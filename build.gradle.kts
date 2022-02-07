1b6e74c555bf61cbbb8f5c2734012bdec2030ee2
Vitaliy Doskoch <vitaliy.doskoch@jmind.systems>
2022-02-07 23:07:02 +0200

    migrated top-level buildscript to kts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2")
+//        classpath "de.mannodermaus.gradle.plugins:android-junit5:1.6.0.0"
+    }
+}
+
+extra.set("kotlin_version", "1.6.10")
+
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+}
+
+apply(from = "$rootDir/processors.gradle")
+apply(from = "$rootDir/libraries.gradle")
+apply(from = "$rootDir/testLibraries.gradle")

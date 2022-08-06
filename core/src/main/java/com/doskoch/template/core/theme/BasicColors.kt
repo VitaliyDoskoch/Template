2980ed92025fbefb0e35157b9bc2b05725a5c1c4
Vitaliy <vitalii.doskoch@jmind.systems>
2022-08-07 00:41:15 +0300

    implemented the SplashScreen layout

--- core/src/main/java/com/doskoch/template/core/theme/BasicColors.kt
+++ core/src/main/java/com/doskoch/template/core/theme/BasicColors.kt
@@ -4,7 +4,11 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val BasicLightColors = lightColors()
+val BasicLightColors = lightColors(
+    primary = Color(0xFF9C27B0),
+    primaryVariant = Color(0xFFBA68C8),
+    secondary = Color(0xFFFFCA28)
+)
 
 val BasicDarkColors = darkColors()
 
--- features/splash/src/main/java/com/doskoch/template/features/splash/SplashScreen.kt
+++ features/splash/src/main/java/com/doskoch/template/features/splash/SplashScreen.kt
@@ -2,24 +2,29 @@ package com.doskoch.template.features.splash
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.doskoch.legacy.android.viewModel.viewModelFactory
+import com.doskoch.template.core.theme.Dimensions
 
 @Composable
 fun SplashScreen() {
@@ -47,7 +52,46 @@ fun SplashScreen(vm: SplashViewModel) {
                     .size(140.dp)
             )
 
-            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = stringResource(R.string.app_name),
+                modifier = Modifier
+                    .padding(start = Dimensions.space_medium, end = Dimensions.space_medium)
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.weight(2f))
+
+            Text(
+                text = LocalContext.current.run {
+                    stringResource(R.string.version, packageManager.getPackageInfo(packageName, 0).versionName)
+                },
+                modifier = Modifier
+                    .padding(start = Dimensions.space_medium, end = Dimensions.space_medium)
+                    .wrapContentWidth()
+                    .background(color = MaterialTheme.colors.secondary, shape = MaterialTheme.shapes.large)
+                    .padding(horizontal = Dimensions.space_medium, vertical = Dimensions.space_small_x6),
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = stringResource(R.string.developed_by),
+                modifier = Modifier
+                    .padding(
+                        start = Dimensions.space_medium,
+                        top = Dimensions.space_medium,
+                        end = Dimensions.space_medium,
+                        bottom = Dimensions.space_large_x2
+                    )
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }

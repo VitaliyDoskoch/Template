eea53aeded674d58a7635dee60013424f7e1328a
Vitaliy <vitalii.doskoch@jmind.systems>
2022-08-07 00:04:04 +0300

    added the icon

--- features/splash/src/main/java/com/doskoch/template/features/splash/SplashScreen.kt
+++ features/splash/src/main/java/com/doskoch/template/features/splash/SplashScreen.kt
@@ -1,14 +1,21 @@
 package com.doskoch.template.features.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.doskoch.legacy.android.viewModel.viewModelFactory
 
@@ -21,14 +28,23 @@ fun SplashScreen() {
 
 @Composable
 fun SplashScreen(vm: SplashViewModel) {
-    Scaffold {
-        Box(
+    Scaffold { paddingValues ->
+        Column(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Blue),
-            contentAlignment = Alignment.Center
+                .padding(paddingValues)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = vm.text)
+            Spacer(modifier = Modifier.weight(1f))
+
+            Image(
+                painter = painterResource(R.drawable.im_app_icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(64.dp)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }

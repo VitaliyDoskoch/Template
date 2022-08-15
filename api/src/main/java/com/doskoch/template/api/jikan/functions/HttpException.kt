47e714efaa4cfbafb2cdc5f5d1710b45009c2093
Vitaliy <vitalii.doskoch@jmind.systems>
2022-08-15 17:54:03 +0300

    fixed HttpException

--- api/src/main/java/com/doskoch/template/api/jikan/functions/HttpException.kt
+++ api/src/main/java/com/doskoch/template/api/jikan/functions/HttpException.kt
@@ -5,5 +5,9 @@ import com.doskoch.template.api.jikan.common.error.ErrorResponse
 import retrofit2.HttpException
 
 fun HttpException.errorResponse() = response()?.errorBody()?.string()?.let {
-    JikanApiProvider.gson.fromJson(it, ErrorResponse::class.java)
+    try {
+        JikanApiProvider.gson.fromJson(it, ErrorResponse::class.java)
+    } catch (t: Throwable) {
+        null
+    }
 }

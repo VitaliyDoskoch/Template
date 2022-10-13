ada1313fc267a9e149993e7727c216e49562a65f
Vitaliy <vitalii.doskoch@jmind.systems>
2022-10-13 20:06:39 +0300

    removed logging

--- core/src/main/java/com/doskoch/template/core/components/paging/SimpleInMemoryStorage.kt
+++ core/src/main/java/com/doskoch/template/core/components/paging/SimpleInMemoryStorage.kt
@@ -5,7 +5,6 @@ import androidx.paging.PagingState
 import timber.log.Timber
 import kotlin.math.min
 
-//TODO: remove logging
 class SimpleInMemoryStorage<K : Any, V : Any> {
 
     private val invalidationCallbacks = mutableSetOf<() -> Unit>()
@@ -68,14 +67,13 @@ class SimpleInMemoryStorage<K : Any, V : Any> {
         private val items: List<V> by lazy { pages.values.filterNotNull().flatten() }
 
         init {
-            val invalidationCallback = { Timber.e("invalidation"); invalidate() }
+            val invalidationCallback = { invalidate() }
 
             registerInvalidatedCallback { invalidationCallbacks.remove(invalidationCallback) }
             invalidationCallbacks.add(invalidationCallback)
         }
 
         override fun getRefreshKey(state: PagingState<Int, V>): Int? = state.anchorPosition
-            ?.also { Timber.e("anchorPosition: $it") }
             ?.coerceAtMost(items.lastIndex)
             ?.let { it - state.config.initialLoadSize / 2 }
             ?.coerceAtLeast(0)
@@ -85,26 +83,15 @@ class SimpleInMemoryStorage<K : Any, V : Any> {
             val limit = if (params is LoadParams.Prepend) min(key, params.loadSize) else params.loadSize
             val offset = if (params is LoadParams.Prepend) (key - params.loadSize).coerceAtLeast(0) else key
 
-            LoadResult.Page(
+            val result = LoadResult.Page(
                 data = items.drop(offset).take(limit),
                 prevKey = offset.takeIf { it > 0 },
                 nextKey = (offset + limit).takeIf { it in items.indices },
                 itemsBefore = offset,
                 itemsAfter = (items.size - (offset + limit)).coerceAtLeast(0)
-            ).also {
-                Timber.e(
-                    "key = $key, limit = $limit, offset = $offset, " +
-                        "items = ${it.data.size}, storageItems = ${items.size}, " +
-                        "prevKey = ${it.prevKey}, nextKey = ${it.nextKey}, " +
-                        "itemsBefore = ${it.itemsBefore}, itemsAfter = ${it.itemsAfter}"
-                )
-            }
-                .let {
-                    if (invalid) {
-                        Timber.e("invalid")
-                        LoadResult.Invalid()
-                    } else it
-                }
+            )
+
+            if (invalid) LoadResult.Invalid() else result
         } catch (t: Throwable) {
             Timber.e(t)
             LoadResult.Error(t)

58d4d9f9317289b1dd80e40eb41262d5fcfcffcb
Vitaliy <vitalii.doskoch@jmind.systems>
2022-10-20 14:22:37 +0300

    added the alpha feature

--- core/src/main/java/com/doskoch/template/core/ui/modifier/SimpleVerticalScrollbar.kt
+++ core/src/main/java/com/doskoch/template/core/ui/modifier/SimpleVerticalScrollbar.kt
@@ -6,56 +6,43 @@ import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListLayoutInfo
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlin.math.abs
 
-fun Modifier._simpleVerticalScrollbar(
-    state: LazyListState,
-    width: Dp = 8.dp
-): Modifier = composed {
-    val targetAlpha = 1f//if (state.isScrollInProgress) 1f else 0f
-    val duration = if (state.isScrollInProgress) 150 else 500
-
-    val alpha by animateFloatAsState(
-        targetValue = targetAlpha,
-        animationSpec = tween(durationMillis = duration)
-    )
-
-    drawWithContent {
-        drawContent()
-
-        val firstVisibleElementIndex = state.layoutInfo.visibleItemsInfo.firstOrNull()?.index
-        val needDrawScrollbar = true//state.isScrollInProgress || alpha > 0.0f
-
-        // Draw scrollbar if scrolling or if the animation is still running and lazy column has content
-        if (needDrawScrollbar && firstVisibleElementIndex != null) {
-            val elementHeight = this.size.height / state.layoutInfo.totalItemsCount
-            val scrollbarOffsetY = firstVisibleElementIndex * elementHeight
-            val scrollbarHeight = state.layoutInfo.visibleItemsInfo.size * elementHeight
-
-            drawRect(
-                color = Color.Red,
-                topLeft = Offset(this.size.width - width.toPx(), scrollbarOffsetY),
-                size = Size(width.toPx(), scrollbarHeight),
-                alpha = alpha
-            )
-        }
-    }
-}
+private const val DISPLAY_TIME = 1_000L
 
 fun Modifier.simpleVerticalScrollbar(state: LazyListState) = composed {
     val color = MaterialTheme.colors.primary
 
+    val targetAlpha = remember(state) { mutableStateOf(1f) }
+
+    val alpha by animateFloatAsState(
+        targetValue = targetAlpha.value,
+        animationSpec = tween(durationMillis = if (targetAlpha.value == 1f) 0 else 500)
+    )
+
+    LaunchedEffect(state.layoutInfo.totalItemsCount, state.isScrollInProgress) {
+        targetAlpha.value = 1f
+        delay(DISPLAY_TIME)
+        ensureActive()
+        targetAlpha.value = if (state.isScrollInProgress) 1f else 0f
+    }
+
     drawWithContent {
         drawContent()
+
+        if (alpha == 0f) return@drawWithContent
 
         val info = state.layoutInfo
 
@@ -90,7 +77,8 @@ fun Modifier.simpleVerticalScrollbar(state: LazyListState) = composed {
         drawRoundRect(
             color = color,
             topLeft = Offset(size.width - widthPx, y),
-            size = Size(widthPx, heightPx)
+            size = Size(widthPx, heightPx),
+            alpha = alpha
         )
     }
 }

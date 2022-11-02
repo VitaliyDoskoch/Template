95d72e841eeb9c7422a32eefde279cccf848383a
Vitaliy <vitalii.doskoch@jmind.systems>
2022-11-02 15:53:29 +0200

    refactored the app di

--- core/src/main/java/com/doskoch/template/core/components/error/GlobalErrorHandler.kt
+++ core/src/main/java/com/doskoch/template/core/components/error/GlobalErrorHandler.kt
@@ -1,5 +1,9 @@
 package com.doskoch.template.core.components.error
 
 interface GlobalErrorHandler {
+    /**
+     * @param showIfNotHandled whether to show the error on the ui if no specific action is required
+     * @return false if no action is performed
+     */
     fun handle(error: CoreError, showIfNotHandled: Boolean = true): Boolean
 }

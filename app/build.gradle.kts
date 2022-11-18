d713fb429b4cce13e97c30234aa6b6891c19bb3e
Vitaliy <vitalii.doskoch@jmind.systems>
2022-11-18 15:41:01 +0200

    moved hook install script to root gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,13 +20,6 @@ android {
     }
 }
 
-val installGitHook: Task by tasks.creating {
-    println("Copying files from the 'rootDir/git-hooks' folder into the '.git/hooks' folder")
-    copy { from("$rootDir/git-hooks").into("$rootDir/.git/hooks") }
-}
-
-tasks.getByPath(":app:preBuild").dependsOn(installGitHook)
-
 dependencies {
     implementation(project(":api"))
     implementation(project(":database"))
--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,10 @@ buildscript {
 tasks {
     register("clean", Delete::class) { delete(rootProject.buildDir) }
 }
+
+val installGitHook: Task by tasks.creating {
+    println("Copying files from the 'rootDir/git-hooks' folder into the '.git/hooks' folder")
+    copy { from("$rootDir/git-hooks").into("$rootDir/.git/hooks") }
+}
+
+tasks.getByPath(":app:preBuild").dependsOn(installGitHook)

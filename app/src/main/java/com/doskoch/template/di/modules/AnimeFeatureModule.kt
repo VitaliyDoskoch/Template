f2a5a5bd699bedf280e9d79142f7a8d56f316576
Vitaliy <vitalii.doskoch@jmind.systems>
2023-01-16 18:04:11 +0200

    created the test FeatureComponent within AnimeFeatureModule.kt

--- app/src/main/java/com/doskoch/template/di/modules/AnimeFeatureModule.kt
+++ app/src/main/java/com/doskoch/template/di/modules/AnimeFeatureModule.kt
@@ -4,6 +4,15 @@ import com.doskoch.template.anime.di.AnimeFeatureComponent
 import com.doskoch.template.anime.navigation.AnimeFeatureNavigator
 import com.doskoch.template.api.jikan.di.JikanApiModule
 import com.doskoch.template.di.AppComponent
+import dagger.hilt.DefineComponent
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Scope
+import javax.inject.Singleton
 
 fun animeFeatureModule(component: AppComponent) = object : AnimeFeatureComponent {
     override val navigator = object : AnimeFeatureNavigator() {
@@ -17,4 +26,45 @@ fun animeFeatureModule(component: AppComponent) = object : AnimeFeatureComponent
     override val dbAnimeDao = component.appDatabase.dbAnimeDao()
 
     override val animeService = JikanApiModule.animeService
+}
+
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class FeatureScope
+
+@FeatureScope
+@DefineComponent(parent = SingletonComponent::class)
+interface FeatureComponent {
+
+    @DefineComponent.Builder
+    interface Builder {
+        fun build(): FeatureComponent
+    }
+
+    @EntryPoint
+    @InstallIn(FeatureComponent::class)
+    interface FeatureEntryPoint {
+        fun getRandomInt(): Int
+    }
+}
+
+@Singleton
+class Manager @Inject constructor(
+    private val provider: Provider<FeatureComponent.Builder>
+) {
+    var component: FeatureComponent? = null
+        private set
+
+    private fun createComponent() {
+        component = provider.get().build()
+    }
+
+    private fun destroyComponent() {
+        component = null
+    }
+}
+
+fun access(manager: Manager) {
+    EntryPoints.get(manager.component!!, FeatureComponent.FeatureEntryPoint::class.java).getRandomInt()
 }
